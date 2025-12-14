50d9b78adf833124b00f9bca8666b0abc9867a95
Vaishnaviwasankar <79964006+Vaishnaviwasankar@users.noreply.github.com>
2025-12-14 23:44:54 +0530

    Add files via upload

---
+++ src/main/java/com/javaguides/kafka_tutorial/kafka/kafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.javaguides.kafka_tutorial.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class kafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(kafkaConsumer.class);
+
+    @KafkaListener(topics = "javaguides", groupId = "myGroup")
+    public void consumer(String message){
+        LOGGER.info(String.format("Message received -> %s", message));
+    }
+
+}
